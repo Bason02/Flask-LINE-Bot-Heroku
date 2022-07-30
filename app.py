ec5c27d7b56ec8d20069844c1d50ee7feeea5ea6
Bason02 <99241288+Bason02@users.noreply.github.com>
2022-07-30 14:34:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+
 from flask import Flask, abort, request
 
 # https://github.com/line/line-bot-sdk-python
@@ -8,12 +9,15 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
+
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
+nlist=[{"國名":"美國","code":"USA","面積":"平方千米"},{"國名":"英國","code":"BPD","面積":"平方千米"},{"國名":"中國","code":"CHN","面積":"平方千米"},{"國名":"台國","code":"TWC","面積":"平方千米"},{"國名":"紐西蘭","code":"NCL","面積":"平方千米"},{"國名":"日本","code":"JPY","面積":"平方千米"},{"國名":"加國","code":"CAD","面積":"平方千米"}]
 
 @app.route("/", methods=["GET", "POST"])
 def callback():
+
     if request.method == "GET":
         return "Hello Heroku"
     if request.method == "POST":
@@ -26,7 +30,8 @@ def callback():
             abort(400)
 
         return "OK"
- 
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
@@ -34,4 +39,3 @@ def handle_message(event):
     # Send To Line
     reply = TextSendMessage(text=f"{get_message}")
     line_bot_api.reply_message(event.reply_token, reply)
-
